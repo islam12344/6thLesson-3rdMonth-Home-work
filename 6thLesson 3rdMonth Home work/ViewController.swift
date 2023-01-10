075b6f77ee594305f4b9e7d255c04e3eac297cdf
Islam Arliev <islam.arliev2611@gmail.com>
2023-01-11 00:10:44 +0600

    Initial Commit

---
+++ 6thLesson 3rdMonth Home work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  6thLesson 3rdMonth Home work
+//
+//  Created by Islam on 11/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
